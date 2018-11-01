4cb0a6dc2e889666502ed39133b896cf0203381b
David Van Der Beek <earlynovrock@gmail.com>
2018-11-01 11:14:57 -0500

    remove in_root

--- template.rb
+++ template.rb
@@ -144,11 +144,9 @@ end
 def add_accounts
   generate "model Account name owner:belongs_to"
 
-  in_root do
-    migration = Dir.glob("db/migrate/*").max_by{ |f| File.mtime(f) }
-    puts "MIGRATION FOR ACCOUNTS:::::>>>>#{migration}"
-    gsub_file migration, /foreign_key: true/, "foreign_key: { to_table: :users }"
-  end
+  migration = Dir.glob("db/migrate/*").max_by{ |f| File.mtime(f) }
+  puts "MIGRATION FOR ACCOUNTS:::::>>>>#{migration}"
+  gsub_file migration, /foreign_key: true/, "foreign_key: { to_table: :users }"
 end
 
 def add_administrate
