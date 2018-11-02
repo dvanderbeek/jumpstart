f07436f99483f124f31af94421c47e90428ec953
David Van Der Beek <earlynovrock@gmail.com>
2018-11-02 07:43:37 -0500

    Fix initializer

--- config/initializers/saas.rb
+++ config/initializers/saas.rb
@@ -1,6 +1,6 @@
 Saas.configure do |config|
   env = Rails.env.production? ? :production : :development
-  rails_config = Rails.application.configuration[env]
+  rails_config = Rails.application.credentials[env]
   if rails_config && rails_config[:stripe]
     config.stripe_public_key = rails_config[:stripe][:public_key]
     config.stripe_secret_key = rails_config[:stripe][:secret_key]
